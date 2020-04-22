0520487e0f05b1d96591f7436307b69bebc1ab50
yltx <>
2020-04-22 20:03:40 +0800

    no message

---
+++ GrandauntApp/GrandauntApp/DepartMentPage/Controllers/DepartMentViewController.m
@@ -0,0 +1,32 @@
+//
+//  StudyViewController.m
+//  GrandauntApp
+//
+//  Created by yltx on 2020/4/22.
+//  Copyright © 2020 grandaunt. All rights reserved.
+//
+
+#import "DepartMentViewController.h"
+
+@interface DepartMentViewController ()
+
+@end
+
+@implementation DepartMentViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
